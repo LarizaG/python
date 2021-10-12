54005fd6855a7814f9951b246bfaae05fd5e743d
Astrti Lariza Garcia Castillo <90290485+LarizaG@users.noreply.github.com>
2021-10-12 18:55:41 -0500

    hola mundo
    hola mundo en python desde vim

---
+++ holapython.py
@@ -0,0 +1,9 @@
+# Hola Mundo en Python
+# Titulo:       :Hola Mundo en Python
+# Description   :Hola Mundo en Python desde vim
+# Author        :Astrti Lariza Garcia Castillo
+# Fecha         :20211005
+# Version       :1
+# Usage         :c:\>python /path/holapython.py
+
+print ("hola mundo desde python")
